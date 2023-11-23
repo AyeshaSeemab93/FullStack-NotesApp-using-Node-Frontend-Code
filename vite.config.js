09a1a561f010dda777bc07d5679b6062e5e3e5fe
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-11-23 12:12:58 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  "base": "/Notes-App-FullStack-React-FrontEnd-Part/",
+  "base": "/FullStack-NotesApp-using-Node-Frontend-Code/",
   plugins: [react()],
 })
